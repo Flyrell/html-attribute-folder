823746e2358b5a6a3765086e00a58ecb12d5e43d
Thomas Scharke <scharke@van.dermeer.de>
2024-02-27 19:17:45 +0100

    feat: Toggle expand/collapse of defined HTML attributes with keystroke

---
+++ src/main/kotlin/dev/zbinski/htmlattributefolder/AttributeFolderAction.kt
@@ -0,0 +1,41 @@
+package dev.zbinski.htmlattributefolder
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.editor.FoldRegion
+
+class AttributeFolderAction : AnAction() {
+    private val settings = AttributeFolderState.instance
+
+    private fun getFoldRegionsForRelevantAttributes(foldRegions: Array<FoldRegion>): List<FoldRegion> {
+        val relevantAttributes = settings.attributes
+
+        // Identifying a fold based on its "groupName"/"debugName" is possible because all folds relevant
+        // to us have a self-defined `FoldingGroup` including the "groupname"/"debugName".
+        // (see ->AttributeFolder.buildFoldRegions:37)
+        return foldRegions.filter {
+            val group = it.group
+            // The `group.toString()` returns the "groupName"/"debugName"
+            group != null && relevantAttributes.contains(group.toString())
+        }
+
+    }
+
+    override fun actionPerformed(e: AnActionEvent) {
+        val editor = e.getData(CommonDataKeys.EDITOR)
+        val project = e.getData(CommonDataKeys.PROJECT)
+        if (editor == null || project == null) {
+            return
+        }
+
+        ApplicationManager.getApplication().runWriteAction {
+            settings.collapse = !settings.collapse
+            val foldRegions = editor.foldingModel.allFoldRegions
+            editor.foldingModel.runBatchFoldingOperation {
+                getFoldRegionsForRelevantAttributes(foldRegions).forEach { fold -> fold.isExpanded = !fold.isExpanded }
+            }
+        }
+    }
+}
