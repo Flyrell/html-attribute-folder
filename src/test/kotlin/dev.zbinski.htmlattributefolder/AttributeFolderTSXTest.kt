579e55f733cee107cf8899dbcb6a3c808f51882c
Thomas Scharke <thomas.scharke@dm.de>
2025-12-17 17:03:59 +0100

    fix: fold TSX object literal attributes
    Fixes #14

--- src/test/kotlin/dev.zbinski.htmlattributefolder/AttributeFolderTSXTest.kt
+++ src/test/kotlin/dev.zbinski.htmlattributefolder/AttributeFolderTSXTest.kt
@@ -2,7 +2,7 @@ package dev.zbinski.htmlattributefolder
 
 private const val TSX_SNIPPET = """
     export const Component = () => (
-      <div class="a b" className="c d" style={{ backgroundColor: "red" }} data-foo="foo-data" />
+      <div class="a b" className="c d" style={{ backgroundColor: "red", nested: { "foo": "bar" } }} data-foo="foo-data" />
     )
 """
 
@@ -10,30 +10,30 @@ class AttributeFolderTSXTest : BaseAttributeFolderTest() {
     fun testFoldingTSXAttributesCollapsed() {
         assertContains(document.text, """class="a b"""")
         assertContains(document.text, """className="c d"""")
-        assertContains(document.text, """style={{ backgroundColor: "red" }}""")
+        assertContains(document.text, """style={{ backgroundColor: "red", nested: { "foo": "bar" } }}""")
         assertContains(document.text, """data-foo="foo-data"""")
 
-        configureAttributeFolder(collapseByDefault = true)
+        configureAttributeFolder(collapseByDefault = true, arrayListOf("class", "className", "style"))
 
         val visualText = applyPluginFoldingAndRender()
         assertContains(visualText, """class="__PLACEHOLDER__"""")
         assertContains(visualText, """className="__PLACEHOLDER__"""")
-        assertContains(visualText, """style={{ backgroundColor: "red" }}""")
+        assertContains(visualText, """style={{__PLACEHOLDER__}}""")
         assertContains(visualText, """data-foo="foo-data"""")
     }
 
     fun testFoldingTSXAttributesUncollapsed() {
         assertContains(document.text, """class="a b"""")
         assertContains(document.text, """className="c d"""")
-        assertContains(document.text, """style={{ backgroundColor: "red" }}""")
+        assertContains(document.text, """style={{ backgroundColor: "red", nested: { "foo": "bar" } }}""")
         assertContains(document.text, """data-foo="foo-data"""")
 
-        configureAttributeFolder(collapseByDefault = false)
+        configureAttributeFolder(collapseByDefault = false, arrayListOf("class", "className", "style"))
 
         val visualText = applyPluginFoldingAndRender()
         assertContains(visualText, """class="a b"""")
         assertContains(visualText, """className="c d"""")
-        assertContains(visualText, """style={{ backgroundColor: "red" }}""")
+        assertContains(visualText, """style={{ backgroundColor: "red", nested: { "foo": "bar" } }}""")
         assertContains(visualText, """data-foo="foo-data"""")
     }
 
